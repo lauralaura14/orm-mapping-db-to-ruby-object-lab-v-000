1eda9e7002d87141023536d3d52e51ff969d6ab5
Laura <laurao148@gmail.com>
2018-04-06 23:47:12 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -15,7 +15,10 @@ class Student
       FROM students
       SQL
 
-      DB[:conn].execute(sql)
+      DB[:conn].execute(sql).map do |row|
+        self.new_from_db(sql)
+      end
+
   end
 
   def self.find_by_name(name)
