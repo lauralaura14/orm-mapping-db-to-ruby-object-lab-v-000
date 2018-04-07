a0183163fc35f8d2f09d09890b226715fd6f59e4
Laura <laurao148@gmail.com>
2018-04-07 00:05:19 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -46,6 +46,15 @@ class Student
   end
 
   def self.students_below_12th_grade
+    sql = <<-SQL
+      SELECT COUNT(*)
+      FROM students
+      WHERE grade = '9'
+    SQL
+
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
 
   def self.first_X_students_in_grade_10
